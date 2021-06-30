a1bee0e1b9009f5de557e211dde8af9ee34f7429
MTmatao <1414294651@qq.com>
2021-06-30 10:36:10 +0800

    commit

--- src/components/TagView/index.tsx
+++ src/components/TagView/index.tsx
@@ -1,48 +1,44 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { RouteContext, RouteContextType } from '@ant-design/pro-layout';
+import { RouteContext } from '@ant-design/pro-layout';
+import type { RouteContextType } from '@ant-design/pro-layout';
 import { history } from 'umi';
 import Tags from './tags';
 import styles from './index.less';
 
 export type TagsItemType = {
-  title: string;
-  path: string;
+  title?: string;
+  path?: string;
   actived: boolean;
   query?: any;
   children: any;
 };
 
-console.log('zhixingl');
-// antd-design-pro-v5-tabview
 /**
  * @component TagView 标签页组件
  */
 const TagView: React.FC = ({ children }) => {
   const [tags, setTags] = useState<TagsItemType[]>([]);
 
-  const routeContext = useRef<RouteContextType>();
+  const routeContextRef = useRef<RouteContextType>();
 
   useEffect(() => {
-    console.log(routeContext);
-  }, []);
-
-  useEffect(() => {
-    console.log(routeContext);
-    if (routeContext?.current) {
-      handleOnChange(routeContext.current);
+    if (routeContextRef?.current) {
+      handleOnChange(routeContextRef.current);
     }
-  }, [routeContext?.current]);
+  }, [routeContextRef?.current]);
 
   // 初始化 visitedViews，设置project为首页
   const initTags = (routeContext: RouteContextType) => {
     if (tags.length === 0 && routeContext.menuData) {
       const firstTag = routeContext.menuData.filter((el) => el.path === '/welcome')[0];
+      const title = firstTag.name;
+      const path = firstTag.path;
       setTags([
         {
-          title: firstTag.name + '',
-          path: firstTag.path + '',
-          actived: true,
+          title,
+          path,
           children,
+          actived: true,
         },
       ]);
       history.push({ pathname: firstTag.path, query: firstTag.query });
@@ -51,49 +47,55 @@ const TagView: React.FC = ({ children }) => {
 
   // 关闭标签
   const handleCloseTag = (tag: TagsItemType) => {
-    let tagsCopy: TagsItemType[] = tags.map((el) => ({ ...el }));
-    // 判断是否已打开过该页面
-    tagsCopy.forEach((item, i) => {
-      if (item.path === tag?.path) {
-        tagsCopy[i - 1].actived = true;
-      } else {
-        item.actived = false;
-      }
-    });
-    setTags(tagsCopy.filter((el) => el.path !== tag?.path));
+    // 判断关闭标签是否处于打开状态
+    const tagsCopy: TagsItemType[] = tags
+      .map((el, i) => {
+        if (el.path === tag.path) {
+          const path = tags[i - 1].path || '';
+          history.push(path);
+        }
+        return { ...el };
+      })
+      .filter((el) => el.path !== tag?.path);
+
+    setTags(tagsCopy);
   };
 
   // 监听路由改变
   const handleOnChange = (routeContext: RouteContextType) => {
+    const { currentMenu } = routeContext;
+
+    // tags初始化
     if (tags.length === 0) {
-      return initTags(routeContext);
+      initTags(routeContext);
+      return false;
     }
 
-    const { currentMenu } = routeContext;
-    let tagsCopy: TagsItemType[] = tags.map((el) => ({ ...el }));
-    let hasTag = false;
-
     // 判断是否已打开过该页面
-    tagsCopy.forEach((item) => {
+    let hasOpen = false;
+    const tagsCopy: TagsItemType[] = tags.map((item) => {
       if (currentMenu?.path === item.path) {
-        item.actived = true;
-        hasTag = true;
+        hasOpen = true;
+        return { ...item, actived: true };
       } else {
-        item.actived = false;
+        return { ...item, actived: false };
       }
     });
 
-    // 没有该tag时追加一个
-    if (!hasTag) {
+    // 没有该tag时追加一个,并打开这个tag页面
+    if (!hasOpen) {
+      const title = routeContext.title || '';
+      const path = currentMenu?.path;
       tagsCopy.push({
-        title: routeContext?.title + '',
-        path: currentMenu?.path + '',
-        actived: true,
+        title,
+        path,
         children,
+        actived: true,
       });
     }
 
     setTags(tagsCopy);
+    return false;
   };
 
   return (
@@ -101,7 +103,7 @@ const TagView: React.FC = ({ children }) => {
       <RouteContext.Consumer>
         {(value: RouteContextType) => {
           // console.log(value);
-          routeContext.current = value;
+          routeContextRef.current = value;
           return null;
         }}
       </RouteContext.Consumer>
